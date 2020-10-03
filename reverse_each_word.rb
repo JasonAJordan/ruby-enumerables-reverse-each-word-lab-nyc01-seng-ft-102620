83e578e3c56037e84e8067cc7d5ad1e0b56220e0
Jason Jordan <jasonajordan0@gmail.com>
2020-10-03 15:31:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,15 @@ def reverse_each_word(string)
   array = string.split(" ")
   
   array.each{|word|
-  
+    count = 0
     newWord = ''
     
+    while word.length > count do 
+      newWord.unshift(word[count])
+    end 
+    word = newWord
+  }
+  
+  newString = array.join(" ")
+end 
   
